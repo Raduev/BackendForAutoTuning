2d34e316aad113ff330070484b1db1c8f350e54a
Husein <ahmadov.m48@gmail.com>
2022-01-05 17:39:59 +0300

    добавил ключи firstName, lastName

--- models/User.model.js
+++ models/User.model.js
@@ -1,9 +1,26 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-  login: String,
-  password: String,
-  number: String,
+  login: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  number: {
+    type: String,
+    required: true,
+  },
 });
 
 const User = mongoose.model("User", userSchema);
