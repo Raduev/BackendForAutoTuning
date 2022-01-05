87099f9c961c968d1f70f24a921d07c85c90cd7f
tazurkaev_halid <barzaev97@gmail.com>
2022-01-05 14:43:23 +0300

    add fix

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -2,36 +2,39 @@ const Cart = require('../models/Cart.model')
 const jwt = require('jsonwebtoken')
 module.exports.cartController = {
 
-  getCart: async (req,res) => {
+  getCart: async (req, res) => {
     try {
       const cart = await Cart.find()
       res.json(cart)
-    } catch (e){
+    }
+    catch (e) {
       res.json(e)
     }
   },
-  addCart: async (req,res) => {
+  addCart: async (req, res) => {
     try {
-     await  Cart.create({
-       user: req.body.user,
-       auto: req.body.auto,
-       service: req.body.service,
-       master: req.body.master
+      await Cart.create({
+        user: req.body.user,
+        auto: req.body.auto,
+        service: req.body.service,
+        master: req.body.master
       })
       res.json('Корзина создана')
-    } catch (e) {
+    }
+    catch (e) {
       res.json(e)
     }
   },
-  removeCart: async (req,res) => {
+  removeCart: async (req, res) => {
     try {
       await Cart.findByIdAndRemove(req.params.id)
       res.json('Корзина удалена')
-    } catch (e) {
+    }
+    catch (e) {
       res.json(e)
     }
   },
-  patchCart: async (req,res) => {
+  patchCart: async (req, res) => {
     try {
       await Cart.findByIdAndUpdate(req.params.id, {
         user: req.body.user,
@@ -40,33 +43,36 @@ module.exports.cartController = {
         master: req.body.master
       })
       res.json('Корзина изменена')
-    } catch (e) {
+    }
+    catch (e) {
       res.json(e)
     }
   },
-  cartToken: async (req,res) => {
+  cartToken: async (req, res) => {
     try {
-      const {user, auto, service, master} = req.body
+      const { user, auto, service, master } = req.body
 
-      const {authorization} = req.headers
+      const { authorization } = req.headers
 
       const [type, token] = authorization.split(' ');
 
-      if(type !== "Bearer") {
+      if (type !== "Bearer") {
         return res.status(400).json('Неверный тип покена')
       }
 
-        const payload = await jwt.verify(token, process.env.SECRET_JWT_KEY)
+      const payload = await jwt.verify(token, process.env.SECRET_JWT_KEY)
 
-        const cart = await Cart.create({
-          user: payload.id,
-          auto,
-          service,
-          master
-        })
+      const cart = await Cart.create({
+        user: payload.id,
+        auto,
+        service,
+        master
+      })
       return res.json(cart)
-      } catch (e) {
-        res.status(401).json('Неверный токен')
-  }
+    }
+    catch (e) {
+      res.status(401).json('Неверный токен')
+    }
 
+  }
 }
