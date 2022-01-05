62ce2e1533a860b712405f7ce4ff71cf0c62762f
tazurkaev_halid <barzaev97@gmail.com>
2022-01-05 17:22:43 +0300

    1

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -31,12 +31,12 @@ module.exports.cartController = {
     try {
       const cart = await Cart.findById(id)
 
-      if(req.user.id !== cart.user) {
+      if (req.user.id !== cart.user) {
         return res.status(401).json('Ошибка. Нет доступа')
       }
 
 
-      if(cart.user.toString() === req.user.id) {
+      if (cart.user.toString() === req.user.id) {
         await cart.remove()
         return res.json('Удалено')
       }
@@ -44,56 +44,60 @@ module.exports.cartController = {
     }
     catch (e) {
       res.status(401).json('Ошибка:' + e.toString())
-  addCart: async (req,res) => {
-    try {
-     await  Cart.create({
-       user: req.body.user,
-       auto: req.body.auto,
-       service: req.body.service,
-       master: req.body.master
-      })
-      res.json('Корзина создана')
-    } catch (e) {
-      res.json(e)
     }
   },
+      addCart: async (req, res) => {
+        try {
+          await Cart.create({
+            user: req.body.user,
+            auto: req.body.auto,
+            service: req.body.service,
+            master: req.body.master
+          })
+          res.json('Корзина создана')
+        }
+        catch (e) {
+          res.json(e)
+        }
+      },
 
-  patchCart: async (req,res) => {
-    try {
-      await Cart.findByIdAndUpdate(req.params.id, {
-        user: req.body.user,
-        auto: req.body.auto,
-        service: req.body.service,
-        master: req.body.master
-      })
-      res.json('Корзина изменена')
-    } catch (e) {
-      res.json(e)
-    }
-  },
-  cartToken: async (req,res) => {
-    try {
-      const {user, auto, service, master} = req.body
+        patchCart: async (req, res) => {
+        try {
+          await Cart.findByIdAndUpdate(req.params.id, {
+            user: req.body.user,
+            auto: req.body.auto,
+            service: req.body.service,
+            master: req.body.master
+          })
+          res.json('Корзина изменена')
+        }
+        catch (e) {
+          res.json(e)
+    }},
+        cartToken: async (req, res) => {
+        try {
+          const { auto, service, master } = req.body
 
-      const {authorization} = req.headers
+          const { authorization } = req.headers
 
-      const [type, token] = authorization.split(' ');
+          const [type, token] = authorization.split(' ');
 
-      if(type !== "Bearer") {
-        return res.status(400).json('Неверный тип покена')
+          if (type !== "Bearer") {
+            return res.status(400).json('Неверный тип покена')
+          }
+
+          const payload = await jwt.verify(token, process.env.SECRET_JWT_KEY)
+
+          const cart = await Cart.create({
+            user: payload.id,
+            auto,
+            service,
+            master
+          })
+          return res.json(cart)
+        }
+        catch (e) {
+          res.status(401).json('Неверный токен')
+        }
       }
-
-        const payload = await jwt.verify(token, process.env.SECRET_JWT_KEY)
-
-        const cart = await Cart.create({
-          user: payload.id,
-          auto,
-          service,
-          master
-        })
-      return res.json(cart)
-      } catch (e) {
-        res.status(401).json('Неверный токен')
-  }
-  }
-}
+    }
