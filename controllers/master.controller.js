fed79bc5d53859078767a6f635ab68108d680638
Ibragim <mih230903@gmail.com>
2022-01-05 10:51:27 +0300

    Master

---
+++ controllers/master.controller.js
@@ -0,0 +1,42 @@
+const Master = require("../models/Master.model");
+
+module.exports.masterController = {
+  getMaster: async (req, res) => {
+    try {
+      const getMaster = await Master.find();
+      res.json(getMaster);
+    } catch (error) {
+      res.json(error.message);
+    }
+  },
+  postMaster: async (req, res) => {
+    try {
+      await Master.create({
+        name: req.body.name,
+        price: req.body.price,
+      });
+      res.json("Мастер успешно добавлен");
+    } catch (error) {
+      res.json(error.message);
+    }
+  },
+  patchMaster: async (req, res) => {
+    try {
+      await Master.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        price: req.body.price,
+      });
+      res.json("Данные мастера успешно изменены");
+    } catch (error) {
+      res.json(error.message);
+    }
+  },
+  deleteMaster: async (req, res) => {
+    try {
+      await Master.findByIdAndRemove(req.params.id);
+      res.json("Данные мастера успешно удалены");
+    } catch (error) {
+      res.json(error.message);
+    }
+  },
+};
