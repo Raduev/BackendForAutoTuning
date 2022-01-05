82d86e5c8147642796dd728b684e524897ece063
Raduev <dominator95rrr@gmail.com>
2022-01-05 09:51:57 +0300

    fix Service

--- controllers/service.controller.js
+++ controllers/service.controller.js
@@ -3,10 +3,11 @@ const Service = require("../models/Service.model");
 module.exports.controllerService = {
   postService: async (req, res) => {
     try {
-      const { time, user } = req.body;
+      const { time, user, img } = req.body;
       const service = await Service.create({
         time: time,
         user: user,
+        img: img
       });
       res.json(service);
     } catch (e) {
@@ -31,10 +32,11 @@ module.exports.controllerService = {
   },
   patchService: async (req, res) => {
     try {
-      const { time, user } = req.body;
+      const { time, user, img } = req.body;
       const service = await Service.findByIdAndUpdate(req.params.id, {
         time: time,
         user: user,
+        img: img
       });
       res.json(service);
     } catch (e) {
