3b9fe671ebf04a5dc171ca65b0c52e6f444ea17b
tazurkaev_halid <barzaev97@gmail.com>
2022-01-05 16:17:38 +0300

    final cart commit

--- routes/cart.route.js
+++ routes/cart.route.js
@@ -1,13 +1,13 @@
 const {cartController} = require('../controllers/cart.controller')
 const {Router} = require('express')
-
+const autMiddleware = require('../models/midlewares/auth.middleware')
 const router = Router()
 
 
-router.get('/cart/:id', cartController.getCart)
-router.post('/cart', cartController.addCart)
-router.delete('/cart/:id', cartController.removeCart)
-router.patch('/cart/:id', cartController.patchCart)
-router.post('/cartToken', cartController.cartToken)
+router.get('/cart/:id',autMiddleware, cartController.getCart)
+router.post('/cart',autMiddleware, cartController.addCart)
+router.delete('/cart/:id',autMiddleware, cartController.removeCart)
+router.patch('/cart/:id',autMiddleware, cartController.patchCart)
+router.post('/cartToken',autMiddleware, cartController.cartToken)
 
 module.exports = router;
